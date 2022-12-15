5b6f3fa61e73657b8b20a31e69e21824f16360b3
DinnerPlz <joeld@qed-electronics.com>
2022-12-15 02:25:11 -0800

    Transform pos intheritance compolete

--- Hell/Main.cs
+++ Hell/Main.cs
@@ -2,9 +2,11 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
-
+using Hell.Source.Engine.Rendering;
+using Hell.Source.Engine;
+using Hell.Source.Engine.Components;
 using System;
+using Unity.Mathematics;
 
 
 
@@ -27,6 +29,29 @@ namespace Hell
             // TODO: Add your initialization logic here
 
             Debug.Logger.InitializeLogger();
+            Graphics.InitializeGraphics();
+
+
+            //manual stuff for testing
+            gameObject game1 = new gameObject();
+            gameObject game2 = new gameObject();
+
+            Transform t1 = game1.transform;
+            Transform t2 = game2.transform;
+
+            t1.localPosition = new float3(1, 2, 3);
+            t2.localPosition = new float3(5, 5, 3);
+
+            t2.SetParent(t1);
+
+            Debug.Logger.LogInfo(t1.localPosition.ToString());
+            Debug.Logger.LogInfo(t2.localPosition.ToString());
+           
+            t1.ResolveTransform();
+            t1.ResolveChildrenTransform();
+
+            Debug.Logger.LogInfo(t1.position.ToString());
+            Debug.Logger.LogInfo(t2.position.ToString());
 
 
             base.Initialize();
@@ -43,6 +68,11 @@ namespace Hell
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            // manual bs (remove)
+
+
+
 
             // TODO: Add your update logic here
 
